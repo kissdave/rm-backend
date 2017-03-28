af217a2f6eda6751fb03b82ce66609472b9c6060
kissdave <kiss.david@simonyi.bme.hu>
2017-03-28 03:30:29 +0200

    Hello World (with mvc)

---
+++ src/main/java/hu/bme/simonyi/dave/controller/HomeCtrl.java
@@ -0,0 +1,16 @@
+package hu.bme.simonyi.dave.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by dkiss on 2017. 03. 16..
+ */
+@Controller
+public class HomeCtrl {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "view";
+    }
+}
