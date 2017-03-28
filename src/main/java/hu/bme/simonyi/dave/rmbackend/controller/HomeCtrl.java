f389a48f7b4f55ac53e41261752f8a3d2b7be9fe
kissdave <kiss.david@simonyi.bme.hu>
2017-03-28 13:10:52 +0200

    Controller packages fixed

--- src/main/java/hu/bme/simonyi/dave/rmbackend/controller/HomeCtrl.java
+++ src/main/java/hu/bme/simonyi/dave/rmbackend/controller/HomeCtrl.java
@@ -1,4 +1,4 @@
-package hu.bme.simonyi.dave.controller;
+package hu.bme.simonyi.dave.rmbackend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
